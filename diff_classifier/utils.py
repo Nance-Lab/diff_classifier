fe4fd9c2cf89bd7fe195761b99ce4f45755ce067
Chad Curtis <ccurtis7@uw.edu>
2018-01-22 23:50:16 +0000

    Added doc strings to a few new functions.

--- diff_classifier/utils.py
+++ diff_classifier/utils.py
@@ -18,6 +18,27 @@ def csv_to_pd(csvfname):
     return data
 
 def partition_im(tiffname, irows=4, icols=4, ires=512):
+    """
+    partition_im(tiffname, irows=int, icols=int, ires=int)
+    
+    Partitions a 2048x2044 image into irows x icols images of size ires x ires and saved them.
+    
+    Parameters
+    ----------
+    tiffname : string
+        Location of input image to be partitioned.
+    irows : int
+        Number of rows of size ires pixels to be partitioned from source image.
+    icols : int
+        Number of columns of size ires pixels to be partitioned from source image.
+    ires : int
+        Output images are of size ires x ires pixels.
+    
+    Examples
+    ----------
+    >>> partition_im('your/sample/image.tif', irows=8, icols=8, ires=256)
+    
+    """
     test = sio.imread(tiffname)
     test2 = np.zeros((test.shape[0], 2048, 2048), dtype=test.dtype)
     test2[:, 0:2044, :] = test
@@ -28,3 +49,43 @@ def partition_im(tiffname, irows=4, icols=4, ires=512):
         for col in range(icols):
             new_image = test2[:, row*ires:(row+1)*ires, col*ires:(col+1)*ires]
             sio.imsave(tiffname.split('.tif')[0] + '_%s_%s.tif'%(row, col), new_image)
+
+def nth_diff(dataframe, n = 1):
+    """
+    nth_diff(dataframe, n=int)
+    
+    Returns a new vector of size N - n containing the nth difference between vector elements.
+    
+    Parameters
+    ----------
+    dataframe : pandas column of floats or ints
+        input data on which differences are to be calculated.
+    n : int, default is 1
+        Function calculated x(i) - x(i - n) for all values in pandas column
+    
+    Returns
+    ----------
+    diff : pandas column
+        Pandas column of size N - n, where N is the original size of dataframe.
+    
+    Examples
+    ----------
+    >>> d = {'col1': [1, 2, 3, 4, 5]}
+    >>> df = pd.DataFrame(data=d)
+    >>> nth_diff(df['col1'], 1)
+    0   1
+    1   1
+    2   1
+    3   1
+    Name: col1, dtype: int64
+    
+    >>> nth_diff(df['col1'], 2)
+    0   2
+    1   2
+    2   2
+    Name: col1, dtype: int64
+    """
+    test1 = dataframe[:-n].reset_index(drop=True)
+    test2 = dataframe[n:].reset_index(drop=True)
+    diff = test2 - test1
+    return diff
