6d10c2ebac35002b5d8554a986241425af4ce94d
arokem <arokem@gmail.com>
2018-01-23 14:11:37 -0800

    Try to use the Fiji bin from the ImageJ website.

--- diff_classifier/tests/test_imagej.py
+++ diff_classifier/tests/test_imagej.py
@@ -1,5 +1,11 @@
 import diff_classifier.imagej as ij
-
+import os.path as op
+import tempfile
+import pandas as pd
 
 def test_run_tracking():
-    ij.track('http://fiji.sc/samples/FakeTracks.tif', 'foo.csv')
+    tf  = tempfile.NamedTemporaryFile(suffix='.csv')
+    ij.track('http://fiji.sc/samples/FakeTracks.tif', tf.name)
+    assert op.exists(tf.name)
+
+    df = pd.read_csv(tf.name)
