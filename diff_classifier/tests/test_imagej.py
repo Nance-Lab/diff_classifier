c5b74fb1596a75cf6ca8e38039e35c9a121f8e6c
arokem <arokem@gmail.com>
2018-01-23 14:17:48 -0800

    Read the result and assert on its shape.

--- diff_classifier/tests/test_imagej.py
+++ diff_classifier/tests/test_imagej.py
@@ -2,10 +2,12 @@ import diff_classifier.imagej as ij
 import os.path as op
 import tempfile
 import pandas as pd
+from diff_classifier.utils import csv_to_pd
 
 def test_run_tracking():
     tf  = tempfile.NamedTemporaryFile(suffix='.csv')
     ij.track('http://fiji.sc/samples/FakeTracks.tif', tf.name)
     assert op.exists(tf.name)
 
-    df = pd.read_csv(tf.name)
+    df = csv_to_pd(tf.name)
+    assert df.shape == (84, 8)
