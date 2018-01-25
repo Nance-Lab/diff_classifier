f2187f35e9b2e69ec20ce67b8f49ecccaa96b2bc
arokem <arokem@gmail.com>
2018-01-25 10:09:08 -0800

    Skip if we're running on the CI, or if we're running this on a Mac.

--- diff_classifier/tests/test_imagej.py
+++ diff_classifier/tests/test_imagej.py
@@ -1,9 +1,16 @@
+import os
+import pytest
+import sys
 import diff_classifier.imagej as ij
 import os.path as op
 import tempfile
-import pandas as pd
 from diff_classifier.utils import csv_to_pd
 
+is_travis = "CI" in os.environ.keys()
+is_mac = sys.platform == "darwin"
+
+@pytest.mark.skipif(is_travis, reason="We're running this on Travis")
+@pytest.mark.skipif(is_mac, reason="This doesn't work on Macs yet")
 def test_run_tracking():
     tf  = tempfile.NamedTemporaryFile(suffix='.csv')
     ij.track('http://fiji.sc/samples/FakeTracks.tif', tf.name)
