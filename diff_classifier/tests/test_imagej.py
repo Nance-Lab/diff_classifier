e493725b392bd703f90f6876e87c88eca76d522c
arokem <arokem@gmail.com>
2018-01-25 10:09:37 -0800

    Whitespace.

--- diff_classifier/tests/test_imagej.py
+++ diff_classifier/tests/test_imagej.py
@@ -9,6 +9,7 @@ from diff_classifier.utils import csv_to_pd
 is_travis = "CI" in os.environ.keys()
 is_mac = sys.platform == "darwin"
 
+
 @pytest.mark.skipif(is_travis, reason="We're running this on Travis")
 @pytest.mark.skipif(is_mac, reason="This doesn't work on Macs yet")
 def test_run_tracking():
