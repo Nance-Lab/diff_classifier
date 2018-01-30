98eea9ccd604bde9734a73a69b84baf3965c1fcd
Chad Curtis <ccurtis7@uw.edu>
2018-01-30 11:30:02 -0800

    Fixed unit tests.

--- diff_classifier/tests/test_msd.py
+++ diff_classifier/tests/test_msd.py
@@ -24,8 +24,11 @@ def test_msd_calc():
          'X': [5, 6, 7, 8, 9],
          'Y': [6, 7, 8, 9, 10]}
     df = pd.DataFrame(data=d)
+    msd, gauss = msd_calc(df)
 
-    npt.assert_equal(np.array([0, 2, 8, 18, 32]).astype('float64'), msd_calc(df))
+    npt.assert_equal(np.array([0, 2, 8, 18, 32]).astype('float64'), msd)
+    npt.assert_equal(np.array([0, 0.25, 0.25, 0.25, 0.25]).astype('float64'), gauss)
+    
 
 
 def test_all_msds():
@@ -35,5 +38,14 @@ def test_all_msds():
          'X': [5, 6, 7, 8, 9, 1, 2, 3, 4, 5],
          'Y': [6, 7, 8, 9, 10, 2, 3, 4, 5, 6]}
     df = pd.DataFrame(data=d)
+    
+    di = {'Frame': [1, 2, 3, 4, 5, 1, 2, 3, 4, 5],
+          'Track_ID': [1, 1, 1, 1, 1, 2, 2, 2, 2, 2],
+          'X': [5, 6, 7, 8, 9, 1, 2, 3, 4, 5],
+          'Y': [6, 7, 8, 9, 10, 2, 3, 4, 5, 6],
+          'MSDs': [float(i) for i in[0, 2, 8, 18, 32, 0, 2, 8, 18, 32]],
+          'Gauss': [0, 0.25, 0.25, 0.25, 0.25, 0, 0.25, 0.25, 0.25, 0.25]}
+    cols = ['Frame', 'Track_ID', 'X', 'Y', 'MSDs', 'Gauss']
+    dfi = pd.DataFrame(data=di)[cols]
 
-    npt.assert_equal(np.array([0., 2., 8., 18., 32., 0., 2., 8., 18., 32.]).astype('float64'), all_msds(df))
+    pdt.assert_frame_equal(dfi, all_msds(df))
