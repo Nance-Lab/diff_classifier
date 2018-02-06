fadd37689b15920ec5ee83111520c18d413c4119
arokem <arokem@gmail.com>
2018-02-06 14:13:55 -0800

    Commenting out the skips, so that Travis tries to test this.

--- diff_classifier/tests/test_imagej.py
+++ diff_classifier/tests/test_imagej.py
@@ -10,8 +10,8 @@ is_travis = "CI" in os.environ.keys()
 is_mac = sys.platform == "darwin"
 
 
-@pytest.mark.skipif(is_travis, reason="We're running this on Travis")
-@pytest.mark.skipif(is_mac, reason="This doesn't work on Macs yet")
+# @pytest.mark.skipif(is_travis, reason="We're running this on Travis")
+# @pytest.mark.skipif(is_mac, reason="This doesn't work on Macs yet")
 def test_run_tracking():
     tf  = tempfile.NamedTemporaryFile(suffix='.csv')
     ij.track('http://fiji.sc/samples/FakeTracks.tif', tf.name)
