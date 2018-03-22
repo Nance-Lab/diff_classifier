6f4942bb7ecb1388e72abaf398d36dfe5de92a51
Chad Curtis <ccurtis7@uw.edu>
2018-03-22 13:20:05 -0700

    Added regression function for predicting tracking params.

--- diff_classifier/imagej.py
+++ diff_classifier/imagej.py
@@ -5,6 +5,9 @@ import skimage.io as sio
 import subprocess
 import tempfile
 import diff_classifier as dc
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import linear_model
+from sklearn import svm
 
 
 def partition_im(tiffname, irows=4, icols=4, ires=512):
@@ -42,19 +45,19 @@ def partition_im(tiffname, irows=4, icols=4, ires=512):
             current = tiffname.split('.tif')[0] + '_%s_%s.tif' % (row, col)
             sio.imsave(current, new_image)
             names.append(current)
-    
+
     return names
 
 
 def mean_intensity(local_im):
     test_image = sio.imread(local_im)
     test_intensity = np.mean(test_image[0, :, :])
-    
+
     return test_intensity
 
 
-def track(target, out_file, template=None, fiji_bin=None, radius=2.5, threshold=5., 
-          do_median_filtering=False, quality=30.0, x=511, y=511, ylo=1, median_intensity=55000.0, snr=0.0, 
+def track(target, out_file, template=None, fiji_bin=None, radius=2.5, threshold=5.,
+          do_median_filtering=False, quality=30.0, x=511, y=511, ylo=1, median_intensity=55000.0, snr=0.0,
           linking_max_distance=10.0, gap_closing_max_distance=10.0, max_frame_gap=3,
           track_displacement=0.0):
     """
@@ -97,3 +100,42 @@ def track(target, out_file, template=None, fiji_bin=None, radius=2.5, threshold=
     fid = open(out_file, 'w')
     fid.write(sp.stdout.decode())
     fid.close()
+
+
+def regress_tracking_params(all_videos, y, training_size, have_output=True):
+    """
+    Uses regression techniques to select the best tracking parameters.
+    Regression again intensities of input images.
+    """
+
+    tprefix = []
+    for i in range(0, tnum):
+        random.seed(i+1)
+        tprefix.append(to_track[random.randint(0, len(to_track))])
+        if have_output is False:
+            print("Get parameters for: {}".format(tprefix[i]))
+
+    if have_output is True:
+        # Define descriptors
+        descriptors = np.zeros((tnum, 4))
+        counter = 0
+        for name in tprefix:
+            local_im = name + '.tif'
+            test_image = sio.imread(local_im)
+            descriptors[counter, 0] = np.mean(test_image[0, :, :])
+            descriptors[counter, 1] = np.std(test_image[0, :, :])
+            descriptors[counter, 2] = np.percentile(test_image[0, :, :], 10)
+            descriptors[counter, 3] = np.percentile(test_image[0:, :, :], 90)
+            counter = counter + 1
+
+        # Define regression techniques
+        X = descriptors
+        classifiers = [
+            svm.SVR(),
+            linear_model.SGDRegressor(),
+            linear_model.BayesianRidge(),
+            linear_model.LassoLars(),
+            linear_model.ARDRegression(),
+            linear_model.PassiveAggressiveRegressor(),
+            linear_model.TheilSenRegressor(),
+            linear_model.LinearRegression()]
