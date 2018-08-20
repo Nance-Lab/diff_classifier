af365ad6f8ed128b79f2ae56c15f4f2ad32f2c48
Chad Curtis <ccurtis7@uw.edu>
2018-08-20 14:05:45 -0700

    Added pca tests.

---
+++ diff_classifier/tests/test_pca.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import pandas.util.testing as pdt
+import numpy.testing as npt
+import diff_classifier.msd as msd
+import diff_classifier.pca as pca
+import diff_classifier.features as ft
+
+
+def test_partial_corr():
+    dataf = msd.random_traj_dataset()
+    msds = msd.all_msds2(dataf, frames=100)
+    feat = ft.calculate_features(msds)
+    npt.assert_equal(24.0, np.round(np.sum(pcorr), 1))
+
+
+def test_kmo():
+    dataf = random_traj_dataset(nparts=300, ndist=(2, 6))
+    msds = msd.all_msds2(dataf, frames=100)
+    feat = ft.calculate_features(msds)
+    dataset = feat.drop(['frames', 'Track_ID'], axis=1)
+    npt.assert_equal(np.round(pca.kmo(dataset), 3), 0.958)
+
+
+def test_pca_analysis():
+    print()
+
+
+def test_plot_pca():
+    print()
